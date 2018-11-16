fa5bd337e0e8cd727ef9473053c3b99b6309396e
Kevin <GreenApple76@users.noreply.github.com>
2018-11-16 16:31:47 -0500

    change svg icon fill method. remove unused font reference

--- js/main.js
+++ js/main.js
@@ -46,31 +46,6 @@ $(document).ready(function(){
     $('#greeting').text('Good evening');
   }
 
-  // replace svg images with inline svg
-  // so that color of svg icons can be
-  // changed with css svg tag
-  $('img[src$=".svg"]').each(function() {
-    var $img = jQuery(this);
-    var imgURL = $img.attr('src');
-    var attributes = $img.prop("attributes");
-
-    $.get(imgURL, function(data) {
-        // Get the SVG tag, ignore the rest
-        var $svg = jQuery(data).find('svg');
-
-        // Remove any invalid XML tags
-        $svg = $svg.removeAttr('xmlns:a');
-
-        // Loop through IMG attributes and apply on SVG
-        $.each(attributes, function() {
-            $svg.attr(this.name, this.value);
-        });
-
-        // Replace IMG with SVG
-        $img.replaceWith($svg);
-    }, 'xml');
-});
-
   // activate hover on touch for mobile devices
   $('body').bind('touchstart', function() {});
 
